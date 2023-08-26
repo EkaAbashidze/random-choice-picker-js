d53d546f3c552c207a1d52001b5155ba6f4d382e
Eka <eka.abashidze@gmail.com>
2023-08-26 20:45:14 +0400

    chore: use prettier for indentations

--- script.js
+++ script.js
@@ -1,34 +1,32 @@
 const tagsEl = document.getElementById("tags");
 const textArea = document.getElementById("textarea");
 
-
 textArea.focus();
 
-
 textArea.addEventListener("keyup", (e) => {
-    createTags(e.target.value);
-
-    if (e.key === "Enter") {
-      setTimeout(() => {
-        e.target.value = "";
-      }, 10)
-      randomSelect();
-    }
-
-
-})
+  createTags(e.target.value);
+  if (e.key === "Enter") {
+    setTimeout(() => {
+      e.target.value = "";
+    }, 10);
+    randomSelect();
+  }
+});
 
 function createTags(input) {
-  const tags = input.split(",").filter(tag => tag.trim() !== " ").map(tag => tag.trim());
+  const tags = input
+    .split(",")
+    .filter((tag) => tag.trim() !== " ")
+    .map((tag) => tag.trim());
   console.log(tags);
   tagsEl.innerHTML = "";
-  
-  tags.forEach(tag => {
+
+  tags.forEach((tag) => {
     const tagEl = document.createElement("span");
     tagEl.classList.add("tag");
     tagEl.innerText = tag;
     tagsEl.appendChild(tagEl);
-  })
+  });
 }
 
 function randomSelect() {
@@ -50,23 +48,18 @@ function randomSelect() {
       const randomTag = pickRandomTag();
       highlightTag(randomTag);
     }, 100);
-
-  }, times * 100)
-
+  }, times * 100);
 }
 
-  function pickRandomTag() {
-    const tags = document.querySelectorAll(".tag");
-    return tags [(Math.floor(Math.random() * tags.length))]
-  }
+function pickRandomTag() {
+  const tags = document.querySelectorAll(".tag");
+  return tags[Math.floor(Math.random() * tags.length)];
+}
 
-  function highlightTag(tag) {
-    tag.classList.add("highlight");
-  }
+function highlightTag(tag) {
+  tag.classList.add("highlight");
+}
 
-
-  function unHighlightTag(tag) {
-    tag.classList.remove("highlight");
-  }
-
-
+function unHighlightTag(tag) {
+  tag.classList.remove("highlight");
+}
